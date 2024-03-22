a7dcccc44c8a15a6d6b5de3833b68e4c7062d255
Sijo K <131980323+k-sijo@users.noreply.github.com>
2024-03-22 12:33:32 +0530

    /component/dashboard-react.js

---
+++ DashboardHeading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DashboardHeading.css'; // Import your CSS file for styling
+
+function DashboardHeading() {
+  return (
+    <div className="dashboard-heading">
+      <h1>Food Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardHeading;
